cfdcde3d90e5942cd6b0980c7d41da35fdc7f97a
Mallory <lukasz.cnota@gmail.com>
2017-02-13 23:14:21 +0100

    Init commit

---
+++ App1-TestVisualClassDesigner/App1-TestVisualClassDesigner/AppControler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App1_TestVisualClassDesigner
+{
+    public class AppControler : App
+    {
+        public new void Print()
+        {
+            Console.Write("Hello from AppControler from Print\n");
+        }
+
+        public void Print2()
+        {
+            Console.Write("Hello from AppControler from Print2\n");
+        }
+    }
+}
---
+++ App1-TestVisualClassDesigner/App1-TestVisualClassDesigner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1_TestVisualClassDesigner
+{
+    public class App
+    {
+        static void Main(string[] args)
+        {
+            App item = new AppControler();
+
+            item.Print();
+            ((AppControler)item).Print();
+            
+
+            Console.ReadLine();
+        }
+
+        public void Print()
+        {
+            Console.Write("Hello World!\n");
+        }
+    }
+}
