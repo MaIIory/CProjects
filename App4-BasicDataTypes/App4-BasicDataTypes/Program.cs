f5249576d234bc5ad2b1ff48fb2e7f84734cdf2c
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-02-24 17:16:23 +0100

    Add App5-ImplicitlyTypedLocalVars and complete App4

--- App4-BasicDataTypes/App4-BasicDataTypes/Program.cs
+++ App4-BasicDataTypes/App4-BasicDataTypes/Program.cs
@@ -17,6 +17,13 @@ namespace App4_BasicDataTypes
             CharFuncionality();
             ParseFromString();
             UseDataAndTimes();
+            BasicStringFunctionality();
+            StringConcatenation();
+            VerbatimString();
+            StringEquality();
+            FunWithStringBuilder();
+            StringInterpolation();
+
             Console.ReadLine();
         }
 
@@ -27,7 +34,7 @@ namespace App4_BasicDataTypes
             int myInt = 0;
             string myString = "This is my string";
             bool b1 = true, b2 = false, b3 = b1;
-            Console.WriteLine("b1: {0}, b2: {1}, b3: {2}",b1,b2,b3);
+            Console.WriteLine("b1: {0}, b2: {1}, b3: {2}", b1, b2, b3);
             b1 = false;
             Console.WriteLine("b1: {0}, b2: {1}, b3: {2}", b1, b2, b3);
             Console.WriteLine("myInt: {0}", myInt);
@@ -41,7 +48,7 @@ namespace App4_BasicDataTypes
             int i = new int();
             double d = new double();
             DateTime dt = new DateTime();
-            Console.WriteLine("b: {0}, i: {1}, d: {2}, dt: {3}",b,i,d,dt);
+            Console.WriteLine("b: {0}, i: {1}, d: {2}, dt: {3}", b, i, d, dt);
         }
 
         static void ObjectFuncionality()
@@ -52,10 +59,10 @@ namespace App4_BasicDataTypes
             int i = 1;
             Console.WriteLine("GetType of i: {0}", i.GetType());
             Console.WriteLine("GetHashCode of i: {0}", i.GetHashCode());
-            Console.WriteLine("GetTypeCode of i: {0}",i.GetTypeCode());
+            Console.WriteLine("GetTypeCode of i: {0}", i.GetTypeCode());
             Console.WriteLine("ToString of i: {0}", i.ToString());
-            Console.WriteLine("Equals(2): {0}",i.Equals(2));
-            Console.WriteLine("CompareTo(2): {0}",i.CompareTo(2));
+            Console.WriteLine("Equals(2): {0}", i.Equals(2));
+            Console.WriteLine("CompareTo(2): {0}", i.CompareTo(2));
         }
 
         static void DataTypeFunctionality()
@@ -63,10 +70,10 @@ namespace App4_BasicDataTypes
             //usage of methods related to numerical data types
             Console.WriteLine("Max of int: {0}", int.MaxValue);
             Console.WriteLine("Min of int: {0}", int.MinValue);
-            Console.WriteLine("Max of double: {0}",double.MaxValue);
-            Console.WriteLine("Min of double: {0}",double.MinValue);
-            Console.WriteLine("double.PositiveInfinity: {0}",double.PositiveInfinity);
-            Console.WriteLine("double.NegativeInfinity: {0}",double.NegativeInfinity);
+            Console.WriteLine("Max of double: {0}", double.MaxValue);
+            Console.WriteLine("Min of double: {0}", double.MinValue);
+            Console.WriteLine("double.PositiveInfinity: {0}", double.PositiveInfinity);
+            Console.WriteLine("double.NegativeInfinity: {0}", double.NegativeInfinity);
             Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
         }
 
@@ -97,7 +104,7 @@ namespace App4_BasicDataTypes
         {
             Console.WriteLine("\n=> Dates and Times");
 
-            Console.WriteLine("Today's date is {0}",DateTime.Now);
+            Console.WriteLine("Today's date is {0}", DateTime.Now);
             DateTime dt = new DateTime(2015, 10, 17); //year, month, day
             Console.WriteLine("The day of {0} is {1}", dt.ToShortDateString(), dt.DayOfWeek);
 
@@ -107,12 +114,102 @@ namespace App4_BasicDataTypes
             TimeSpan ts = new TimeSpan(4, 50, 0); //hours, minutes, seconds
             Console.WriteLine(ts);
 
-            ts = ts.Subtract(new TimeSpan(0,15,0));
+            ts = ts.Subtract(new TimeSpan(0, 15, 0));
             Console.WriteLine(ts);
+        }
 
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("\n=> Basic String Functionality");
+            string firstName = "Lucyn";
 
-           
-            
+            Console.WriteLine("My first name: {0}: ", firstName);
+            Console.WriteLine("My first name has {0} characters", firstName.Length);
+            Console.WriteLine("My first name in uppercase: {0}", firstName.ToUpper());
+            Console.WriteLine("My first name in lowercase: {0}", firstName.ToLower());
+            Console.WriteLine("Does first name containe 'y'?: {0}", firstName.Contains('y'));
+            Console.WriteLine("Change Luc to poi: {0}", firstName.Replace("Luc", "poi"));
+            Console.WriteLine("My first name: {0}: ", firstName);
+        }
+
+        static void StringConcatenation()
+        {
+            Console.WriteLine("\n=> String Concatenation");
+
+            string s1 = "Hello my name is ";
+            string s2 = "Lukasz";
+            string s3 = s1 + s2;
+            Console.WriteLine(s3);
+
+            string s4 = "Hello my name is ";
+            string s5 = "Lukasz";
+            string s6 = String.Concat(s4, s5);
+            Console.WriteLine(s6);
+        }
+
+        static void VerbatimString()
+        {
+            Console.WriteLine("\n=> Verbatim Strings");
+
+            //error
+            //Console.WriteLine("C:\myapp\dir\file");
+            Console.WriteLine(@"C:\myapp\dir\file");
+
+            string myLongString = @"This is a very
+                                                 very
+                                                    very
+                                                       long string";
+            Console.WriteLine(myLongString);
+            Console.WriteLine(@"Cerebus said ""Darrr! Pret-ty sun-sets""");
+        }
+
+        static void StringEquality()
+        {
+            Console.WriteLine("\n=>String Equality");
+            string s1 = "Hello!";
+            string s2 = "Yo!";
+
+            Console.WriteLine("s1: {0}", s1);
+            Console.WriteLine("s2: {0}", s2);
+            Console.WriteLine();
+
+            //test equality
+            Console.WriteLine("s1 == s2: {0}", s1 == s2);
+            Console.WriteLine("s1 == Hello!: {0}", s1 == "Hello!" );
+            Console.WriteLine("s1 == HELLO!: {0}", s1 == "HELLO!");
+            Console.WriteLine("s1.Equals(s2): {0}",s1.Equals(s2));
+            Console.WriteLine("\"Yo!\".Equals(s2): {0}", "Yo!".Equals(s2));
+        }
+
+        static void StringAreImmutable()
+        {
+            Console.WriteLine("\n=>Strings are immutable, this all you have remembe:)");
+        }
+
+        static void FunWithStringBuilder()
+        {
+            Console.WriteLine("\n=>Using String Builder");
+            StringBuilder sb = new StringBuilder("*** My favorite games ***");
+            sb.Append("\n");
+            sb.AppendLine("Dizzy");
+            sb.AppendLine("empty");
+            sb.AppendLine("Dark Colony");
+            sb.AppendLine("Earth 2140");
+            Console.WriteLine("sb: {0}",sb);
+
+            sb.Replace("empty", "Need for Speed");
+            Console.WriteLine("sb: {0}",sb);
+        }
+
+        static void StringInterpolation()
+        {
+            Console.WriteLine("\n=>String Interpolation");
+            int age = 28;
+            string name = "Lucyn";
+
+            string greeting = $"Hello {name} you are {age} years old.";
+            Console.WriteLine(greeting);
+            Console.WriteLine($"Bye {name.ToUpper()}! You will have {age + 1} next year!");
         }
     }
 }
---
+++ App5-ImplicitlyTypedLocalVars/ImplicitlyTypedLocalVars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitlyTypedLocalVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExplicitVarsDeclaration();
+            ImplicitVarsDeclaration();
+            Console.ReadLine();
+        }
+
+        static void ExplicitVarsDeclaration()
+        {
+            Console.WriteLine("*** Inside ExplicitVarsDeclaration ***");
+            int myInt = 0;
+            bool myBool = true;
+            string myString = "myString";
+
+            Console.WriteLine($"myInt: {myInt.GetType()}");
+            Console.WriteLine($"myBool: {myBool.GetType()}");
+            Console.WriteLine($"myString: {myString.GetType()}");
+            Console.WriteLine();
+        }
+
+        static void ImplicitVarsDeclaration()
+        {
+            Console.WriteLine("*** Inside ImplicitVarsDeclaration ***");
+
+            var myInt = 0;
+            var myBool = true;
+            var myString = "myString";
+
+            Console.WriteLine($"myInt: {myInt.GetType()}");
+            Console.WriteLine($"myBool: {myBool.GetType()}");
+            Console.WriteLine($"myString: {myString.GetType()}");
+            Console.WriteLine();
+        }
+    }
+}
