592ac81d5d34e0a3769b71a10ce09d1c1fc24f69
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-03-03 11:05:23 +0100

    Add App9-Enums, App10-Structures and new image

---
+++ App10-Structures/App10-Structures/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App10_Structures
+{
+    class Program
+    {
+        struct Point
+        {
+            public int X;
+            public int Y;
+
+            public Point (int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Increment()
+            {
+                X++;
+                Y++;
+            }
+
+            public void Decrement()
+            {
+                X--;
+                Y--;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine($"X: {X}, Y: {Y}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** First look at structures ***");
+            Point myPoint;
+            myPoint.X = 340;
+            myPoint.Y = 27;
+
+            myPoint.Print();
+
+            myPoint.Increment();
+            myPoint.Print();
+
+            myPoint.Decrement();
+            myPoint.Print();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ App9-Enums/App9-Enums/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App9_Enums
+{
+    class Program
+    {
+
+        enum EmpType
+        {
+            Manager,        //0
+            LineManager,    //1
+            Programmer,     //2
+            Designer        //3
+        }
+
+        enum HdType
+        {
+            HDisk = 102,    //102
+            RAM,            //103
+            Monitor         //104
+        }
+
+        enum PriceType : long
+        {
+            Fixed = 99,
+            NonFixed = 5
+        }
+
+        static void Main(string[] args)
+        {
+            HdType hdType = HdType.HDisk;
+            AskForHdType(hdType);
+            Console.WriteLine("hdType.toString(): {0} and integer value: {1}",hdType.ToString(),(System.Int32)hdType);
+            Console.WriteLine();
+
+            //Print all possible values for EmpType
+            Console.WriteLine(Enum.GetValues(typeof(EmpType)));
+
+            string[] myStr = new string[10];
+
+            Console.WriteLine("myStr.TypeOf(): {0}", myStr.GetType());
+
+
+            //get full set of EmpType enum
+            Array empArray = Enum.GetValues(typeof(EmpType));
+            Console.WriteLine("empArray.Length: {0}",empArray.Length);
+             
+            for(int i = 0; i < empArray.Length; i++)
+                Console.WriteLine(empArray.GetValue(i));
+
+
+            Console.ReadLine();
+        }
+
+        static void AskForHdType(HdType e)
+        {
+            switch(e)
+            {
+                case HdType.HDisk:
+                    Console.WriteLine("Hard Disk");
+                    break;
+                case HdType.RAM:
+                    Console.WriteLine("RAM Memory");
+                    break;
+                case HdType.Monitor:
+                    Console.WriteLine("Output screen");
+                    break;
+                default:
+                    Console.WriteLine("Unrecognized");
+                    break;
+            }
+        }
+    }
+}
