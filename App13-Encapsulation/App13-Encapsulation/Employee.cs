a4bee0b0d0610229c0a693b99f7dacf7653074ad
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-03-14 16:19:07 +0100

    Add App13-Encapsulation and new image

---
+++ App13-Encapsulation/App13-Encapsulation/Employee.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App13_Encapsulation
+{
+    class Employee
+    {
+        private string empName;
+        private int empId;
+        private float currPay;
+
+        public Employee() { }
+
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empId = id;
+            currPay = pay;
+        }
+
+        public void GiveBonus(float bonus)
+        {
+            currPay += bonus;
+        }
+
+        //Legacy SET implementation
+        public void SetName(string name)
+        {
+            if (name.Length >= 15)
+            {
+                Console.WriteLine("Typed name too long. Name has not been changed!");
+            }
+            else
+            {
+                Console.WriteLine("New name is: {0}", name);
+                empName = name;
+            }
+            return;
+        }
+
+        //Legacy GET implementation
+        public string GetName()
+        {
+            return empName;
+        }
+
+        //GET and SET implementation compliant with .NET properties
+        public int Id
+        {
+            get { return empId; }
+            set
+            {
+                if (value > 100)
+                    Console.WriteLine("Number of employees exceded 100!");
+                else
+                    empId = value;
+            }
+        }
+
+        //GET and SET implementation compliant with .NET properties
+        public float Pay
+        {
+            get { return currPay; }
+            set
+            {
+                if (value < 1200)
+                    Console.WriteLine("Employee can't earn less than 1200!");
+                else
+                    currPay = value;
+            }
+        }
+
+        public void DisplayStatus()
+        {
+            Console.WriteLine("Employee name: {0}", empName);
+            Console.WriteLine("Employee id:   {0}", empId);
+            Console.WriteLine("Employee salary: {0}", currPay);
+        }
+    }
+}
---
+++ App13-Encapsulation/App13-Encapsulation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App13_Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Handling getters and setters in 'old' manner
+            Employee newEmp = new Employee("Lukasz", 12, 1200);
+            newEmp.GiveBonus(1000);
+            newEmp.DisplayStatus();
+            newEmp.SetName("Lucyn");
+            newEmp.SetName("The new name exceeds 15 characters :) ");
+            Console.WriteLine("new employee name: {0}", newEmp.GetName());
+
+            //Handling getters and setters in .NET properties manner
+            newEmp.Id = 200;
+            newEmp.Id = 99;
+            Console.WriteLine("new employee id is: {0}",newEmp.Id);
+
+            newEmp.Pay += 500;
+
+            newEmp.DisplayStatus();
+
+            Console.ReadLine();
+        }
+    }
+}
