6c266cefba0b3f175e73abbe330cd81171dd6243
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-04-11 16:32:08 +0200

    Add App18

---
+++ App18-SimpleException/App18-SimpleException/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App18_SimpleException
+{
+    class Car
+    {
+        public bool isDead = false;
+        public int maxSpeed = 100;
+        public int currentSpeed = 0;
+
+        public void Accelerate(int speed)
+        {
+            currentSpeed += speed;
+            Console.WriteLine("Accelerating to {0}",currentSpeed);
+
+            if(currentSpeed > maxSpeed)
+            {
+                //In case max speed is excedeed, throw new execption
+                currentSpeed = 0;
+                isDead = true;
+                string exceptionMsg = "The car has just blown up";
+                Exception carException = new Exception(exceptionMsg);
+                carException.Data.Add(0, "Car has been damaged permanently");
+
+                throw carException;
+            }
+
+        }
+    }
+}
---
+++ App18-SimpleException/App18-SimpleException/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App18_SimpleException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+
+            //Following part of the code, surely will throw an exception
+            //so the exception object has to be handled 
+
+            try
+            {
+                while (!myCar.isDead)
+                    myCar.Accelerate(10);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("*** EXCEPTION OCCURED ***");
+                Console.WriteLine("Exception message: {0}",e.Message);
+                Console.WriteLine("Exception stack trace: {0}",e.StackTrace);
+                Console.WriteLine("Ex source: {0}", e.Source);
+                Console.WriteLine("Ex TargetSite: {0}",e.TargetSite);
+                Console.WriteLine("Ex data number: {0}",e.Data.Keys.Count);
+
+                foreach(System.Collections.DictionaryEntry de in e.Data)
+                    Console.WriteLine("de key: {0}, de value: {1}", de.Key, de.Value);
+                
+                
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
