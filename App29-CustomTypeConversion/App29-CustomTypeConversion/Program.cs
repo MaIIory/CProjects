7da3b61579cac7a2301312abf32a9bb150aea054
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-07-28 15:59:21 +0200

    Add App29, App30 and wrap up chapter

---
+++ App29-CustomTypeConversion/App29-CustomTypeConversion/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App29_CustomTypeConversion
+{
+    class Program
+    {
+
+        struct Rectangle
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+
+            public Rectangle(int w, int h) : this()
+            {
+                Width = w;
+                Height = h;
+            }
+
+            //Squares can be implicitly converted into Rectangles
+            public static implicit operator Rectangle(Square s)
+            {
+                Rectangle r = new Rectangle(s.Length, s.Length);
+                return r;
+            }
+        }
+
+        struct Square
+        {
+
+            public int x; //not in use - only for learning purpose
+
+            public int Length { get; set; }
+
+            //default constructor must be called to assign defaulr value to x
+            public Square(int len) : this() { Length = len; }
+
+            public override string ToString() { return "Square len: " + Length; }
+
+            
+            // Rectangles can be explicitly converted into Squares. 
+            public static explicit operator Square(Rectangle r)
+            {
+                Square s = new Square();
+                s.Length = r.Height;
+                return s;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Rectangle myRec = new Rectangle(3, 6);
+
+            //Explicit cast
+            Square mySquare = (Square)myRec;
+
+            //Implicit cast (explicit is possible too)
+            Rectangle myRec2 = mySquare;
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ App30-ExtensionMethods/App30-ExtensionMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App30_ExtensionMethods
+{
+
+    //Extend object type
+    static class MyExtensions
+    {
+        public static void DisplayDefiningAssembly(this object obj)
+        {
+            Console.WriteLine("{0} lives here: => {1}\n", obj.GetType().Name, Assembly.GetAssembly(obj.GetType()).GetName().Name);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Now every object can use DisplayDefiningAssembly
+            int x = 5;
+            x.DisplayDefiningAssembly();
+            Console.ReadLine();
+        }
+    }
+}
