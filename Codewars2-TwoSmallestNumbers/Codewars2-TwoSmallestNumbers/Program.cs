fcc8850e24d6e0d5dd53e1beb8d04d0ffdb2bd96
Mallory <lukasz.cnota@gmail.com>
2017-10-10 23:57:31 +0200

    Add some new projects

---
+++ Codewars2-TwoSmallestNumbers/Codewars2-TwoSmallestNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars2_TwoSmallestNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 8, 12, 19, 22 };
+            Console.WriteLine(sumTwoSmallestNumbers(numbers));
+            Console.ReadLine();
+        }
+
+        public static int sumTwoSmallestNumbers(int[] numbers)
+        {
+            Array.Sort(numbers);
+            return numbers[0] + numbers[1];
+
+        }
+    }
+}
