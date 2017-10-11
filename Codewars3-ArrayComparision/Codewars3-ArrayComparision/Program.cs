d4b086a48743058e5c5995454068f10192b6eabe
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-10-11 16:30:50 +0200

    Update Codewars projects

---
+++ Codewars3-ArrayComparision/Codewars3-ArrayComparision/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars3_ArrayComparision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 121, 144, 19, 161, 19, 144, 19, 11 };
+            int[] b = { 121, 14641, 20736, 361, 25921, 361, 20736, 361 };
+
+            Console.WriteLine(comp(a,b));
+            Console.ReadLine();
+        }
+
+        public static bool comp(int[] a, int[] b)
+        {
+            if ((a == null || b == null) || (a.Length != b.Length))
+                return false;
+
+            Array.Sort(a);
+            Array.Sort(b);
+
+            for(int i = 0; i < a.Length; i++)
+                if (b[i] != a[i] * a[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Codewars4-ExesAndOhs/Codewars4-ExesAndOhs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars4_ExesAndOhs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XO("xox");
+            XO("xoxSSS");
+            XO("xox");
+            XO("xoxxOO");
+
+            Console.ReadLine();
+        }
+
+        public static bool XO(string input)
+        {
+            return input.Where(elem => elem == 'x' || elem == 'X').Count() == input.Where(elem => elem == 'o' || elem == 'O').Count();
+        }
+    }
+}
