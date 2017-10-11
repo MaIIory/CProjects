08d682c33362554e2c264c26ed170b468402e13f
Lukasz Cnota <lukasz.cnota@ericsson.com>
2017-10-11 17:19:07 +0200

    Add Codewars5 project solution

---
+++ Codewars5-MultiplesOf3And5/Codewars5-MultiplesOf3And5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars5_MultiplesOf3And5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Solution(10));
+            Console.ReadLine();
+        }
+
+        public static int Solution(int value)
+        {
+            return Enumerable.Range(0, value).Where(elem => elem % 3 == 0 || elem % 5 == 0).Sum();
+        }
+    }
+}
